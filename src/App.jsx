ef56bdcbf5d10b74b1c9ee5f1099ea87f4a2f8ad
vaibhavc13 <v599568@gmail.com>
2024-02-19 10:34:03 +0530

    added pages material,courses,queries,blog pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+
 import Home from "./pages/Home";
+import Queries from "./pages/Queries";
+import Blogs from "./pages/blogs";
+import Materials from "./pages/materials";
+
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <Home />;
+  return (<BrowserRouter>
+      <Routes>
+       <Route path="/study_material_inventory_software" element={<Home />}/>
+       <Route index element={<Home />} />
+       <Route path="/study_material_inventory_software/blogs" element={<Blogs />} />
+          <Route path="/study_material_inventory_software/blogs/Courses" element={<allCourses />} />
+          <Route path="/study_material_inventory_software/blogs/Queries" element={<Queries />} />
+       <Route path="/study_material_inventory_software/materials" element={<Materials />} >
+        {/*   <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />*/}
+        </Route>
+      </Routes>
+    </BrowserRouter>)
+  
 }
 
 export default App;
